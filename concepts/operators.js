1f2db85e6eb8a5a64d2ab555f768c41656c77945
evelynsilverio <sebastiansilverioevelyn@gmail.com>
2024-09-12 13:45:11 -0600

    02 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,35 @@
+// Operadores matemàticos 
+// + y -
+// * y /
+// % y ** Mòdulo y potencia
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+// Operadores de asignaciòn
+let a = 10;
+a += 5; //a = a + 5
+a -= 5; //a = a - 5
+a *= 5; //a = a * 5
+a /= 5; //a = a / 5
+a %= 5; //a = a % 5
+a **= 5; //a = a ** 5
+console.log(a);
+
+// Operadores de Comparaciòn
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 === 2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+// Operadores lògicos
+console.log(true && true); //and
+console.log(true || false); //or
+console.log(!true); //not
