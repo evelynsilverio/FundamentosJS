2795d4b46be40ca82f5affe02222197d1a122c93
evelynsilverio <sebastiansilverioevelyn@gmail.com>
2024-09-12 13:32:35 -0600

    01 Tipos de Datos en Javascript

---
+++ concepts/datatypes.js
@@ -0,0 +1,23 @@
+// Tipos de Dato en Javascript
+// numericos (entero, decimales)
+console.log(23);
+console.log(typeof 23);
+console.log(3.1416);
+console.log(typeof 3.1416);
+
+//booleanos (true, false)
+console.log(true);
+console.log(typeof true);
+console.log(false);
+console.log(typeof false);
+
+console.log('Hello');
+console.log(typeof 'Hello');
+
+console.log(undefined);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof [1 ,2, 3]);
+console.log(typeof {name: 'John' });
+
+
