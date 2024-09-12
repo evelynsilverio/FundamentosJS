9824675b2d04623c336974207ae875718d65155a
evelynsilverio <sebastiansilverioevelyn@gmail.com>
2024-09-12 13:56:57 -0600

    03 Variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,18 @@
+// Declaraciòn de variables con let y const
+// var: en desuso
+
+// variables let: cambiaràn de valor
+let age; //declaraciòn
+console.log(age);
+age = 23; //inicializaciòn
+console.log(age);
+
+let name = "Francisco";// declaraciòn e inicializaciòn
+console.log(name);
+name = "Paco";
+console.log(name);
+
+// constantes const: no cambiaràn de valor
+const PI = 3.1416;
+//PI = 3.15;
+console.log(PI);
